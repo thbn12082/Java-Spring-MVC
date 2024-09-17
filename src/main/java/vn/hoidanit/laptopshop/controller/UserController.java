63aaa207a5f2e4a8e02bc29ec8e5355f3d2f2c3a
thbn12082 <ginnobi1208@gmail.com>
2024-09-17 17:50:22 +0700

    update

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,20 +1,43 @@
 package vn.hoidanit.laptopshop.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.laptopshop.service.UserService;
 
-@RestController
+// @RestController
+// public class UserController {
+//     // làm sao để truy cập method này??? đóa là lí do cần GetMapping
+//     // @Autowired
+//     private UserService userService;
+
+//      public UserController(UserService userService) {
+//         this.userService = userService;
+//      }
+
+//     @GetMapping("/")
+//     public String getHomePage() {
+//         return this.userService.handleHello();
+//     }
+
+// }
+
+@Controller
 public class UserController {
-    // làm sao để truy cập method này??? đóa là lí do cần GetMapping
 
     private UserService userService;
 
-    @GetMapping("/")
-    public String getHomePage() {
-        return "hello from controller";
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
+    @RequestMapping("/")
+    public String getHomePage() {
+        String test = this.userService.handleHello();
+        return "hello";
+    }
 }
