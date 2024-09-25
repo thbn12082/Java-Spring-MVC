8d5212eb2d6facdc45bf9b221f45548054cc62d5
thbn12082 <ginnobi1208@gmail.com>
2024-09-25 14:57:44 +0700

    haha

--- src/main/java/vn/hoidanit/laptopshop/domain_model/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain_model/User.java
@@ -1,7 +1,20 @@
-package vn.hoidanit.laptopshop.domain;
+package vn.hoidanit.laptopshop.domain_model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+// trước khi code, đầu tiên la fphair tạo domain
+
+@Entity
+@Table(name = "User")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String email;
     private String password;
     private String fullname;
---
+++ src/main/java/vn/hoidanit/laptopshop/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package vn.hoidanit.laptopshop.repository;
+
+import org.springframework.stereotype.Repository;
+
+//crud : create read update delete
+@Repository
+public class UserRepository {
+
+}
